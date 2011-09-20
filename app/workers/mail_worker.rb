4d6637fbf405fcf9a71f46ebe76760f9eb5306eb
rkononov <rkononov@masha-PX1.(none)>
2011-09-20 21:40:28 +0600

    fixed actionmailer sending (need to update gem before)

--- app/workers/mail_worker.rb
+++ app/workers/mail_worker.rb
@@ -4,15 +4,9 @@ class MailWorker < SimpleWorker::Base
   attr_accessor :post_id, :to
 
   def run
-
     log 'running...'
-
     @post = Post.find(post_id)
-
-    log 'post=' + @post.inspect
-
-    #Mailer.send_post(@post, to)
-
+    log Mailer.send_post(@post, "to@example.com").deliver!
   end
 
 end
